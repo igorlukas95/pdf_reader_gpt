9027af0c9f1a797f96cccb03662475d03d9c3e9d
igorluk <igorlukas95@gmail.com>
2023-06-07 13:29:29 +0200

    Increased prompt space.

--- my-app/src/Upload.js
+++ my-app/src/Upload.js
@@ -3,61 +3,67 @@ import axios from "axios";
 import Answer from "./Answer";
 
 const Upload = () => {
-    const [file, setFile] = useState(null);
-    const [answer, setAnswer] = useState("");
+  const [file, setFile] = useState(null);
+  const [answer, setAnswer] = useState("");
+  const [question, setQuestion] = useState("");
 
-    const downloadFile = async (filename) => {
-        try {
-            const response = await axios.get(`/download/${filename}`);
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const downloadFile = async (filename) => {
+    try {
+      const response = await axios.get(`/download/${filename}`);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const submitFile = async (event) => {
-        event.preventDefault();
-        try {
-            const formData = new FormData();
-            formData.append("file", file);
-            const response = await axios.post("/upload", formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            });
-            console.log(response.data);
-            downloadFile(file.name);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const submitFile = async (event) => {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const response = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log(response.data);
+      downloadFile(file.name);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const askQuestion = async (inputQuestion) => {
-        try {
-            const response = await axios.post("/question", {
-                question: inputQuestion,
-                filename: file.name,
-            });
-            setAnswer(response.data.answer);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const askQuestion = async () => {
+    try {
+      const response = await axios.post("/question", {
+        question: question,
+        filename: file.name,
+      });
+      setAnswer(response.data.answer);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <div>
-            <input
-                type="file"
-                onChange={(event) => setFile(event.target.files[0])}
-            />
-            <button onClick={submitFile}>Upload</button>
+  return (
+    <div>
+      <input
+        type="file"
+        onChange={(event) => setFile(event.target.files[0])}
+      />
+      <button onClick={submitFile}>Upload</button>
 
-            <input type="text" onChange={(event) => askQuestion(event.target.value)} />
-            <button onClick={() => askQuestion()}>Ask Question</button>
+      <textarea
+        rows="4"
+        cols="50"
+        value={question}
+        onChange={(event) => setQuestion(event.target.value)}
+      ></textarea>
+      <button onClick={askQuestion}>Ask Question</button>
 
-            <Answer answer={answer} />
-        </div>
-    );
+      <Answer answer={answer} />
+    </div>
+  );
 };
 
 export default Upload;
